7bb4b7fe4e25e25207f949dd9c427dad35edf816
papercavegames <111946217+papercavegames@users.noreply.github.com>
2023-03-31 21:29:31 -0700

    Added MAX_FALL_SPEED and clamp fall speed

--- MiniJam129/scenes/game_objects/player/player.gd
+++ MiniJam129/scenes/game_objects/player/player.gd
@@ -4,7 +4,8 @@ extends CharacterBody2D
 const JUMP_VELOCITY = -350.0
 const ACCELERATION = 500.0
 const DECELERATION = 500.0
-const MAX_SPEED = 130.0
+const MAX_SPEED = 160.0
+const MAX_FALL_SPEED = 400
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 980
@@ -14,17 +15,19 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		velocity.y = clamp(velocity.y, -INF, MAX_FALL_SPEED)
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x += direction * ACCELERATION * delta
 		velocity.x = clamp(velocity.x,-MAX_SPEED,MAX_SPEED)
+	
 	else:
 		velocity.x = move_toward(velocity.x, 0, DECELERATION * delta)
 
