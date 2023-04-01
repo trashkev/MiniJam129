21bd6b3e14db499d9fef71118209031ccaa1405b
trashkev <kevasorensen@gmail.com>
2023-03-31 21:05:47 -0700

    Added Acceleration to player.gd

--- MiniJam129/scenes/game_objects/player/player.gd
+++ MiniJam129/scenes/game_objects/player/player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -350.0
+const ACCELERATION = 500.0
+const DECELERATION = 500.0
+const MAX_SPEED = 130.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = 980
 
 
 func _physics_process(delta):
@@ -21,8 +23,9 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x += direction * ACCELERATION * delta
+		velocity.x = clamp(velocity.x,-MAX_SPEED,MAX_SPEED)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, DECELERATION * delta)
 
 	move_and_slide()
