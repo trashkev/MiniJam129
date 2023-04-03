0b6a863da035c9b74a908495cdb31e63c4c00df1
trashkev <kevasorensen@gmail.com>
2023-04-02 17:23:56 -0700

    cutscene

---
+++ MiniJam129/scenes/levels/Cutscene.gd
@@ -0,0 +1,7 @@
+extends Camera2D
+
+func loadNextLevel():
+	var currentLevelIndex = get_tree().current_scene.name.to_int()
+	print("leaving Level_" + str(currentLevelIndex))
+	print("loading Level_" + str(currentLevelIndex+1))
+	get_tree().change_scene_to_file("res://scenes/levels/Level_" + str(currentLevelIndex+1) + ".tscn")
