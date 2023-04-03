1d28103aaeb0d7cd9c88dc6d659543a13dab84eb
trashkev <kevasorensen@gmail.com>
2023-04-02 17:41:19 -0700

    fixed death anim

--- MiniJam129/scenes/levels/Cutscene.gd
+++ MiniJam129/scenes/levels/Cutscene.gd
@@ -5,3 +5,8 @@ func loadNextLevel():
 	print("leaving Level_" + str(currentLevelIndex))
 	print("loading Level_" + str(currentLevelIndex+1))
 	get_tree().change_scene_to_file("res://scenes/levels/Level_" + str(currentLevelIndex+1) + ".tscn")
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("jump"):
+		loadNextLevel()
+	pass
